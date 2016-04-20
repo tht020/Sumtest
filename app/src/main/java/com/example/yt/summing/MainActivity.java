27909d5fc683dcdb39055d84eed6562d1c48f32c
Tim <tht020@ucsd.edu>
2016-04-19 17:27:59 -0700

    Sum stuff

---
+++ app/src/main/java/com/example/yt/summing/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.yt.summing;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    EditText num1;
+    EditText num2;
+    TextView outputtxt;
+    public void buttonOnClick(View v){
+        Button button = (Button) findViewById(R.id.add);
+        num1=(EditText) findViewById(R.id.editText);
+        num2=(EditText) findViewById(R.id.editText2);
+        outputtxt = (TextView) findViewById(R.id.output);
+        int first=Integer.parseInt(num1.getText().toString());
+        int second=Integer.parseInt(num2.getText().toString());
+        int value= first+second;
+        outputtxt.setText(value);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
